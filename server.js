0e52fd443534c02618c09d6fbf25a678dc09788b
Glitch (hello-express) <none>
2018-12-19 16:43:12 +0530

    File added

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 // server.js
 // where your node app starts
-const fun = require('./fun.js')
+const fun = require('./src/fun.js');
 // init project
 const express = require('express');
 const app = express();
 
-// we've started you off with Express, 
+// we've started you off with Express,
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
 // http://expressjs.com/en/starter/static-files.html
@@ -17,17 +17,14 @@ app.get('/', function(request, response) {
 });
 
 app.get('/api/timestamp/:date_string', (req, res) => {
-    let date = req.params.date_string;
-    res.send(fun.check(date)); 
-    
+  let date = req.params.date_string;
+  res.send(fun.check(date));
 });
 
 app.get('/api/timestamp/', (req, res) => {
-   
-        let today = new Date();
-       res.send(fun.check(today.toString()))
-    
-}
+  let today = new Date();
+  res.send(fun.check(today.toString()));
+});
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, function() {
